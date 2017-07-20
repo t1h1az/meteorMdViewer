0208e0628e11f09b4146f9de58c124833b135d9f
Thomas Beetz <beetz.thomas@googlemail.com>
2017-07-20 17:09:24 +0200

    [SUBSCRIPTION] added subscription, returned objects are console.logged

--- client/components/app.js
+++ client/components/app.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './header';
 import Accounts from './accounts';
+import BinsList from './bins/binsList';
 
 export default () => {
   return (
     <div>
       <Header />
+      <BinsList />
       markbin
     </div>
   );
---
+++ client/components/bins/binsList.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import {createContainer} from 'meteor/react-meteor-data';
+import {Bins} from '../../../imports/collections/bins';
+
+class BinsList extends Component {
+  render() {
+    console.log(this.props.bins);
+    return (
+      <div>fuck list</div>
+    );
+  }
+}
+export default createContainer(() => {
+  Meteor.subscribe('bins');
+  return { bins: Bins.find({}).fetch()}
+}, BinsList);
